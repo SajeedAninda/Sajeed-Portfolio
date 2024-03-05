b92a2f79dee6a0c7de9f2a03a07fe211b4f55386
SajeedAninda <sajaninda@gmail.com>
2024-03-05 14:01:35 +0600

    Added a new project

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,41 +5,41 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-import imgA from "../assets/sovereign/a.png"
-import imgB from "../assets/sovereign/b.png"
-import imgC from "../assets/sovereign/c.png"
-import imgD from "../assets/sovereign/d.png"
-import imgE from "../assets/sovereign/e.png"
-import imgF from "../assets/sovereign/f.png"
-import imgG from "../assets/sovereign/g.png"
-import imgH from "../assets/sovereign/h.png"
-import imgI from "../assets/sovereign/i.png"
-import imgJ from "../assets/sovereign/j.png"
-import imgK from "../assets/sovereign/k.png"
-import imgL from "../assets/sovereign/l.png"
-import imgAA from "../assets/delightful/aa.png"
-import imgBB from "../assets/delightful/bb.png"
-import imgCC from "../assets/delightful/cc.png"
-import imgDD from "../assets/delightful/dd.png"
-import imgEE from "../assets/delightful/ee.png"
-import imgFF from "../assets/delightful/ff.png"
-import imgGG from "../assets/delightful/gg.png"
-import imgHH from "../assets/delightful/hh.png"
-import imgII from "../assets/delightful/ii.png"
-import imgJJ from "../assets/delightful/jj.png"
-import imgAAA from "../assets/crafters/aaa-min.png"
-import imgBBB from "../assets/crafters/bbb-min.png"
-import imgCCC from "../assets/crafters/ccc-min.png"
-import imgDDD from "../assets/crafters/ddd-min.png"
-import imgEEE from "../assets/crafters/eee-min.png"
-import imgFFF from "../assets/crafters/fff-min.png"
-import imgGGG from "../assets/crafters/ggg-min.png"
-import imgHHH from "../assets/crafters/hhh-min.png"
-import imgIII from "../assets/crafters/iii-min.png"
-import imgJJJ from "../assets/crafters/jjj-min.png"
-import imgKKK from "../assets/crafters/kkk-min.png"
-import imgLLL from "../assets/crafters/lll-min.png"
-import imgMMM from "../assets/crafters/mmm-min.png"
+import imgA from "../assets/sovereign/a-min.png"
+import imgB from "../assets/sovereign/b-min.png"
+import imgC from "../assets/sovereign/c-min.png"
+import imgD from "../assets/sovereign/d-min.png"
+import imgE from "../assets/sovereign/e-min.png"
+import imgF from "../assets/sovereign/f-min.png"
+import imgG from "../assets/sovereign/g-min.png"
+import imgH from "../assets/sovereign/h-min.png"
+import imgI from "../assets/sovereign/i-min.png"
+import imgJ from "../assets/sovereign/j-min.png"
+import imgK from "../assets/sovereign/k-min.png"
+import imgL from "../assets/sovereign/l-min.png"
+import imgAA from "../assets/delightful/aa-min.png"
+import imgBB from "../assets/delightful/bb-min.png"
+import imgCC from "../assets/delightful/cc-min.png"
+import imgDD from "../assets/delightful/dd-min.png"
+import imgEE from "../assets/delightful/ee-min.png"
+import imgFF from "../assets/delightful/ff-min.png"
+import imgGG from "../assets/delightful/gg-min.png"
+import imgHH from "../assets/delightful/hh-min.png"
+import imgII from "../assets/delightful/ii-min.png"
+import imgJJ from "../assets/delightful/jj-min.png"
+import imgAAA from "../assets/crafters/aaa-min-min.png"
+import imgBBB from "../assets/crafters/bbb-min-min.png"
+import imgCCC from "../assets/crafters/ccc-min-min.png"
+import imgDDD from "../assets/crafters/ddd-min-min.png"
+import imgEEE from "../assets/crafters/eee-min-min.png"
+import imgFFF from "../assets/crafters/fff-min-min.png"
+import imgGGG from "../assets/crafters/ggg-min-min.png"
+import imgHHH from "../assets/crafters/hhh-min-min.png"
+import imgIII from "../assets/crafters/iii-min-min.png"
+import imgJJJ from "../assets/crafters/jjj-min-min.png"
+import imgKKK from "../assets/crafters/kkk-min-min.png"
+import imgLLL from "../assets/crafters/lll-min-min.png"
+import imgMMM from "../assets/crafters/mmm-min-min.png"
 import imgAAAA from "../assets/scholarix/aaaa.png"
 import imgBBBB from "../assets/scholarix/bbbb.png"
 import imgCCCC from "../assets/scholarix/cccc.png"
@@ -53,6 +53,21 @@ import imgJJJJ from "../assets/scholarix/jjjj.png"
 import imgKKKK from "../assets/scholarix/kkkk.png"
 import imgLLLL from "../assets/scholarix/llll.png"
 import imgMMMM from "../assets/scholarix/mmmm.png"
+import imgAAAAA from "../assets/artisan/aaaaa.png"
+import imgBBBBB from "../assets/artisan/bbbbb.png"
+import imgCCCCC from "../assets/artisan/ccccc.png"
+import imgDDDDD from "../assets/artisan/ddddd.png"
+import imgEEEEE from "../assets/artisan/eeeee.png"
+import imgFFFFF from "../assets/artisan/fffff.png"
+import imgGGGGG from "../assets/artisan/ggggg.png"
+import imgHHHHH from "../assets/artisan/hhhhh.png"
+import imgIIIII from "../assets/artisan/iiiii.png"
+import imgJJJJJ from "../assets/artisan/jjjjj.png"
+import imgKKKKK from "../assets/artisan/kkkkk.png"
+import imgLLLLL from "../assets/artisan/lllll.png"
+import imgMMMMM from "../assets/artisan/mmmmm.png"
+import imgNNNNN from "../assets/artisan/nnnnn.png"
+import imgOOOOO from "../assets/artisan/ooooo.png"
 
 import { motion } from 'framer-motion';
 
@@ -149,6 +164,102 @@ const Projects = () => {
 
                                 <SwiperSlide>
                                     <img src={imgMMMM} alt="" />
+                                </SwiperSlide>
+                            </Swiper>
+                        </motion.div>
+                    </div>
+
+                    {/* ARTISAN ALLEY  */}
+                    <div className='flex gap-6 lg:gap-0 flex-col-reverse lg:flex-row items-center pt-8 border-b-2 border-[#f9d5fa] pb-6' data-aos="fade-up-right">
+                        <div className='texts w-full lg:w-[50%] space-y-3'>
+                            <h2 className='text-4xl text-[#151515] font-bold'>Artisan Alley</h2>
+                            <p className='text-xl text-[#151515] font-semibold'>A vibrant marketplace connecting artisans and buyers through a feature-rich full-stack Next.js and Express.js web application.</p>
+                            <ul className='list-disc pl-8 font-medium w-[95%]'>
+                                <li>Secure authentication system with multiple user-role management using Google Firebase</li>
+                                <li>Dynamic parallel product filtering functionality on the products page from the backend.</li>
+                                <li>Convenient booking and management of artisan products, including bookmarking and tracking sales.</li>
+                            </ul>
+
+                            <div className='flex gap-2'>
+                                <a className='font-bold text-lg hover:underline hover:text-[#863d8f]' href="https://artisan-alley-client.vercel.app/" target='_blank'>Live Link</a>
+                                |
+                                <a className='font-bold text-lg hover:underline hover:text-[#863d8f]' href="https://github.com/SajeedAninda/Artisan-Alley-Client" target='_blank'>Client Side</a>
+                                |
+                                <a className='font-bold text-lg hover:underline hover:text-[#863d8f]' href="https://github.com/SajeedAninda/Artisan-Alley-Server" target='_blank'>Server Side</a>
+                            </div>
+                        </div>
+
+                        <motion.div whileHover={{ scale: 1.05 }} className='slides shadow-2xl border-2 p-4 rounded-md border-[#863d8f] w-full lg:w-[50%]'>
+                            <Swiper
+                                spaceBetween={30}
+                                centeredSlides={true}
+                                autoplay={{
+                                    delay: 2500,
+                                    disableOnInteraction: false,
+                                }}
+                                pagination={{
+                                    clickable: true,
+                                }}
+                                navigation={false}
+                                modules={[Autoplay, Pagination, Navigation]}
+                                className="mySwiper shadow-2xl"
+                            >
+                                <SwiperSlide>
+                                    <img src={imgAAAAA} alt="" />
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+                                    <img src={imgBBBBB} alt="" />
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+                                    <img src={imgCCCCC} alt="" />
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+                                    <img src={imgDDDDD} alt="" />
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+                                    <img src={imgEEEEE} alt="" />
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+                                    <img src={imgFFFFF} alt="" />
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+                                    <img src={imgGGGGG} alt="" />
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+                                    <img src={imgHHHHH} alt="" />
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+                                    <img src={imgIIIII} alt="" />
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+                                    <img src={imgJJJJJ} alt="" />
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+                                    <img src={imgKKKKK} alt="" />
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+                                    <img src={imgLLLLL} alt="" />
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+                                    <img src={imgMMMMM} alt="" />
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <img src={imgNNNNN} alt="" />
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <img src={imgOOOOO} alt="" />
                                 </SwiperSlide>
                             </Swiper>
                         </motion.div>
